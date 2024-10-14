dd424eb714b9ee25b8f598b2906e74b37db5b175
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2024-10-14 17:14:06 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18572 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int A =in.nextInt();
+        //int B =in.nextInt();
+
+        for ( int i =1; i<10; i++){
+            System.out.println(A+ " * "+i+" = " + (A*i));
+        }
+
+
+    }
+}
